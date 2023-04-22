947dc510d7df1ce242c7f0beb08733d4b48fbd99
Camila Cristina <camila.alves@nectarcrm.com.br>
2023-04-22 11:11:55 -0300

    project in typescript

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
+import { PencilLine } from 'phosphor-react'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1681195035338-bbdea7af2bf3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      ></img>
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/MyllaC.png" />
+
+        <strong>Camila Cristina</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
